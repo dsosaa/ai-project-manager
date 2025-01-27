2aadac072cc87ba4f09fcec1c471eecc0bf1beba
dsosaa <dsosa@abovebeyondmgmt.com>
2025-01-26 22:54:15 -0800

    Add styles and update components

---
+++ src/components/mindmap/MindMap.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Layout } from "../layout/Layout";
+
+export const MindMap: React.FC = () => {
+  return (
+    <div className="mindmap-container">
+      <h2>Mind Map</h2>
+    </div>
+  );
+};
---
+++ src/components/mindmap/MindMapComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const MindMapComponent = () => {
+  return (
+    <div className="mindmap-container">
+      <h2>AI Project Manager</h2>
+      <div className="mindmap-content">
+        {/* Content will be added here */}
+      </div>
+    </div>
+  );
+};
